68eb0a9bf4821502176ec5b1fd5fdbd34cf508fb
字节君 <96653078+Bytelv@users.noreply.github.com>
2023-11-19 14:45:30 +0800

    Update links.ts

--- config/links.ts
+++ config/links.ts
@@ -27,5 +27,47 @@ export const links : readonly [LinkType, LinkType, LinkType, LinkType, LinkType]
         shadow: "shadow-true-gray-400",
         id: "MannyCooper",
         name: "GitHub"
+    },
+    {
+        url: 'https://sspai.com/u/mannycooper/updates',
+        icon: Sspai,
+        color: 'from-bg-[#d7161c] to-bg-[#fc281a]',
+        fill: "fill-[#da282a]",
+        border: "border-[#da282a]",
+        shadow: "shadow-red-300",
+        text:"text-[#da282a]",
+        name: "少数派"
+    },
+    {
+        // # banned
+        url: 'https://www.zhihu.com/people/zi-jie-jun-83',
+        icon: Zhihu,
+        color: 'from-true-gray-400 to-true-gray-300 dark:(from-true-gray-600 to-true-gray-500)',
+        fill: "fill-[#0084FF]",
+        border: "border-[#0084FF]",
+        shadow: "shadow-blue-300",
+        text:"text-[#0084FF]",
+        id: "optional",
+        name: "知乎"
+    },
+    {
+        url: 'https://dribbble.com/anzifan',
+        icon: Dribbble,
+        fill: "fill-[#EA4C89]",
+        border: "border-[#EA4C89]",
+        shadow: "shadow-pink-300",
+        text:"text-[#EA4C89]",
+        color: 'from-bg-[#ea4c89] to-bg-[#ff589f]',
+        name: 'Dribbble'
+    },
+    {
+        url: 'http://linkedin.com/in/zifan-an/',
+        icon: Linkedin,
+        fill: "fill-[#0A66C2]",
+        border: "border-[#0A66C2]",
+        shadow: "shadow-blue-300",
+        text:"text-[#0A66C2]",
+        color: 'from-bg-[#0b66c2] to-bg-[#008bff]',
+        name: 'LinkedIn'
     }
 ]
